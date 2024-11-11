a805b6cee4d507eb8dbdecf72ca14b270a131309
Gabriel Méthot <gabriel.methot@ziliahealth.com>
2024-11-11 15:06:52 -0500

    launch API before running tests

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 
 app = FastAPI()
 
@@ -6,3 +6,8 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World!"}
+
+
+@app.get("/healthcheck")
+def read_healthcheck():
+    return Response(status_code=200)
